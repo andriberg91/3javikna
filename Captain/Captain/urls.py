4cabc25a2b9891225bee63cb4084542404da8bc8
Sigurður Sigurðsson <sigurdursig17@ru.is>
2020-05-14 17:28:29 +0000

    frontpage 3 items.

--- Captain/Captain/urls.py
+++ Captain/Captain/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('console.urls') ),
+    path('',include('frontpage.urls') ),
     path('admin/', admin.site.urls),
     path('consoles/', include('console.urls')),
     path('manufacturers/', include('manufacturer.urls')),
