ca70d78e65be05333ceec25ab7f4d03532e9eab2
PannaSigrun <annasg19@ru.is>
2020-05-15 20:30:33 +0000

    search

--- Captain/leita/models.py
+++ Captain/leita/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class SearchHistory(models.Model):
+    search_word = models.CharField(max_length=225)
+    time_stamp = models.DateTimeField()
+    user = models.OneToOneField(User, on_delete='SET NULL', blank=True)
+    def __str__(self):
+        return self.search_word
